750aa8660e40615481eca661f419007d7f076399
sonali.raut <sonali.raut@wwindia.com>
2023-08-22 15:12:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const handleInputUsername = (e) => {
+    if (e.target.value) {
+      setEmail(e.target.value);
+      const emailErrorTemp =
+        "" !== e.target.value &&
+        !e.target.value.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+          ? true
+          : false;
+      setEmailError(emailErrorTemp);
+    } else {
+      setEmail("");
+      setEmailError(true);
+    }
+  };
+
+  const handleInputPassword = (e) => {
+    if (e.target.value) {
+      setPassword(e.target.value);
+      const passwordErrorTemp =
+        "" !== e.target.value && e.target.value.length <= 6 ? true : false;
+      setPasswordError(passwordErrorTemp);
+    } else {
+      setPassword("");
+      setPasswordError(true);
+    }
+  };
+  const handleSubmit = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-testid="app">
+      <div className="form_logo">
+        Lo<span>g</span>o
+      </div>
+      <div className="form_title">
+        Log<span>I</span>n
+      </div>
+      <div className="input-container" data-testid="input-username">
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={handleInputUsername}
+          value={email}
+          data-testid="input-email"
+        />
+        <i className="zmdi zmdi-account zmdi-hc-lg"></i>
+        {emailError && (
+          <p data-testid="email-error">Email is required or invalid</p>
+        )}
+      </div>
+
+      <div className="input-container">
+        <input
+          type="password"
+          placeholder="Password"
+          data-testid="input-password"
+          onChange={handleInputPassword}
+          value={password}
+        />
+        <i className="zmdi zmdi-lock zmdi-hc-lg"></i>
+        {passwordError && (
+          <p data-testid="password-error">Password is required or wrong</p>
+        )}
+      </div>
+
+      <button
+        type="button"
+        data-testid="login-button"
+        onClick={handleSubmit}
+        disabled={emailError || password ? true : false}
+      >
+        Log In
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,81 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, wait, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("Should render the app login view", () => {
+    render(<App />);
+    expect(screen.getByTestId("app")).toMatchSnapshot();
+  });
+
+  it("Should render include input as username and password", () => {
+    render(<App />);
+    const loginButton = screen.getByTestId("login-button");
+
+    expect(screen.getByTestId("input-username")).toBeInTheDocument();
+    expect(screen.getByTestId("input-password")).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it("should show no error values when page is been loaded", () => {
+    render(<App />);
+
+    expect(screen.queryByTestId("password-error")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("email-error")).not.toBeInTheDocument();
+  });
+
+  it("should show error for invalid email", () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByTestId("input-email"), {
+      target: { value: "user@" },
+    });
+
+    expect(screen.getByTestId("email-error")).toHaveTextContent(
+      "Email is required or invalid"
+    );
+
+    expect(screen.getByTestId("login-button")).toBeDisabled();
+  });
+
+  it("should show error for invalid password", () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByTestId("input-password"), {
+      target: { value: "user" },
+    });
+
+    expect(screen.getByTestId("password-error")).toHaveTextContent(
+      "Password is required or wrong"
+    );
+
+    expect(screen.getByTestId("login-button")).toBeDisabled();
+  });
+
+  it("should show error for empty value of email", () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByTestId("input-email"), {
+      target: { value: " " },
+    });
+
+    expect(screen.getByTestId("email-error")).toHaveTextContent(
+      "Email is required or invalid"
+    );
+
+    expect(screen.getByTestId("login-button")).toBeDisabled();
+  });
+
+  it("should show error for empty value of password", () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByTestId("input-password"), {
+      target: { value: " " },
+    });
+
+    expect(screen.getByTestId("password-error")).toHaveTextContent(
+      "Password is required or wrong"
+    );
+
+    expect(screen.getByTestId("login-button")).toBeDisabled();
+  });
 });
